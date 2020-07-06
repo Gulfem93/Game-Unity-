55642516096983d4f13ae38a297d7ada53d7a103
Gulfem93 <35203615+Gulfem93@users.noreply.github.com>
2020-07-06 18:24:29 +0300

    Folding Blocks

---
+++ FoldingBlocks/Assets/Code/Bolum1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bolum1 : MonoBehaviour
+{
+    public void sahne1()
+    {
+        SceneManager.LoadScene("Sahne1");
+    }
+    public void sahne2()
+    {
+        SceneManager.LoadScene("Sahne2");
+    }
+    public void sahne3()
+    {
+        SceneManager.LoadScene("Sahne3");
+    }
+    public void sahne4()
+    {
+        SceneManager.LoadScene("Sahne4");
+    }
+}
---
+++ FoldingBlocks/Assets/Code/CopyCube.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyCube : MonoBehaviour
+{
+    public GameObject[] Object;
+    GameObject instance;
+    Vector3 vec;
+
+	void Update ()
+    {
+        if (Input.GetKey("d"))
+        {
+            foreach (GameObject cube in Object)
+            {
+                instance = (GameObject)Instantiate(cube);
+                instance.transform.position = transform.position + new Vector3(1, 0, 0);
+                instance.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+        if (Input.GetKey("a"))
+        {
+            foreach (GameObject cube in Object)
+            {
+                instance = (GameObject)Instantiate(cube);
+                instance.transform.position = transform.position + new Vector3(-1, 0, 0);
+                instance.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+        if (Input.GetKey("w"))
+        {
+            foreach (GameObject cube in Object)
+            {
+                instance = (GameObject)Instantiate(cube);
+                instance.transform.position = transform.position + new Vector3(0, 0, 1);
+                instance.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+        if (Input.GetKey("s"))
+        {
+            foreach (GameObject cube in Object)
+            {
+                instance = (GameObject)Instantiate(cube);
+                instance.transform.position = transform.position + new Vector3(0, 0, -1);
+                instance.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        
+    }
+}
